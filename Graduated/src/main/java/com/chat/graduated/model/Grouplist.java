354283e185f6977fd4602aea8895fdd6fd34e2cc
jiminlim <39897338+jiminlim@users.noreply.github.com>
2019-07-17 16:59:08 +0900

    jimin 17 july

--- Graduated/src/main/java/com/chat/graduated/model/Grouplist.java
+++ Graduated/src/main/java/com/chat/graduated/model/Grouplist.java
@@ -1,5 +1,99 @@
 package com.chat.graduated.model;
 
-public class Grouplist {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.chat.graduated.dao.JDBCUtil;
+import com.chat.graduated.vo.Groupvo;
+import com.chat.graduated.vo.Uservo;
+
+public class Grouplist {
+	PreparedStatement ps =null;
+	Statement sta =null;
+	ResultSet rs =null;
+	String sql=null;
+	Connection con=null;
+	Uservo user= new Uservo();
+	
+	/*
+	public Groupvo groupex(String userid){
+		Groupvo group=new Groupvo();
+		JDBCUtil aa= new JDBCUtil();
+		sql="select * from grouplist where userid=?";
+		try {
+			con=aa.testConnection();
+			System.out.println(con);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}try {
+			ps=con.prepareStatement(sql);
+			ps.setString(1, userid);
+			rs=ps.executeQuery();
+			
+			while(rs.next()){
+				
+				group.setGpin(rs.getString("gpin"));
+				group.setUserid(rs.getString("userid"));
+				group.setName(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			con.close();
+			ps.close();
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return group;
+	}
+*/
+	public List<Groupvo> grouplist( String userid){
+		List<Groupvo> glist=new ArrayList<Groupvo>();
+		JDBCUtil aa= new JDBCUtil();
+		sql="select * from grouplist where userid=?";
+		try {
+			con=aa.testConnection();
+			System.out.println(con);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			ps=con.prepareStatement(sql);
+			ps.setString(1, userid);
+			rs=ps.executeQuery();
+			
+			while(rs.next()){
+				Groupvo vo = new Groupvo();
+				vo.setGpin(rs.getString("gpin"));
+				vo.setUserid(rs.getString("userid"));
+				vo.setName(rs.getString("name"));
+				vo.setPin(rs.getString("pin"));
+				glist.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			con.close();
+			ps.close();
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return glist;
+	}
+	
 }
